7fea09da6039fbe3765b2e7ce5b15f3436c8b4ff
Dominique Sakrisson <domsakrisson@gmail.com>
2019-10-01 21:18:15 -0700

    included errors.php into our register page

--- register.php
+++ register.php
@@ -16,29 +16,39 @@
     </div>
 
     <form class="" action="login.php" method="post">
+
+      <?php include('errors.php') ?>
+
       <span class="logo"><img src="biterlyLogo.png" alt=""></span>
+
       <div class="input-group">
         <label for="username">Username :</label>
         <input type="text" name="username" value="" required>
       </div>
+
       <div class="input-group">
         <label for="email">Email :</label>
         <input type="email" name="email" value="" required>
       </div>
+
       <div class="input-group">
         <label for="password">Password :</label>
         <input type="password" name="password_1" value="" required>
       </div>
+
       <div class="input-group">
         <label for="password">Confirm Password : </label>
         <input type="password" name="password_2" value="" required>
       </div>
+
       <div class="input-group">
         <button type="submit" class="btn" name="reg_user">Sign Up</button>
       </div>
+
       <p>
         Already a member? <a href="login.php">Sign in</a>
       </p>
+
     </form>
   </body>
 </html>
